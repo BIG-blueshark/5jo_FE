2442809e2e071f8c378063920a6b862cd3816d81
BIG-blueshark <blueshark.1949@gmail.com>
2024-04-23 13:24:21 +0900

    변수_test

--- src/App.tsx
+++ src/App.tsx
@@ -3,24 +3,19 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const myVariable = process.env.REACT_APP_MY_VARIABLE;
+    return (
+        <div className="App">
+            <header className="App-header">
+                <img src={logo} className="App-logo" alt="logo" />
+                <p>5조 프론트엔드 리액트 테스트 페이지</p>
+                <a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
+                    Learn React
+                </a>
+                <p>myVariable</p>
+            </header>
+        </div>
+    );
 }
 
 export default App;
